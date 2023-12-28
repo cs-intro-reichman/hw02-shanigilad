de4ece81fe99b8b6c1ca8222f976fa01058bdf38
Shani <shani.gilad@post.runi.ac.il>
2023-12-28 14:02:52 +0200

    question 2

--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,17 @@
  */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
+		String word = args[0];
+		char mid = 0;
+		for (int i = word.length()-1 ; i >= 0 ; i--)
+		{
+			System.out.print(word.charAt(i));
+			if (i == word.length() / 2) 
+			{
+				mid = word.charAt(i);
+			}
+		}
+		System.out.println();
+		System.out.println("The middle character is " + mid);
 	}
 }
