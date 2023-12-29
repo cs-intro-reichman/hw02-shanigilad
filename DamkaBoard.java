03b68358e25b3985a4b2dc169159bcf21d672a72
Shani <shani.gilad@post.runi.ac.il>
2023-12-29 09:34:27 +0200

    task5

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -6,11 +6,15 @@ public class DamkaBoard {
 		int n = Integer.parseInt(args[0]);
 		
 		for(int i = 0 ; i < n ; i++){ // make sure that there is n lines
+				if (i % 2 != 0) {
+				System.out.print(" ") ;
+			}
 			for (int j = 0 ; j <n ; j++){ // prints n stars
 				System.out.print("*") ;
-			}
-				System.out.println() ;// go down a line 
-
+				
+				}
+				System.out.println() ;
+				
 		}
 	}
 }
