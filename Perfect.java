cab71396c04b0a9efe8b5d5f8e60718c67718c55
Shani <shani.gilad@post.runi.ac.il>
2023-12-29 09:02:53 +0200

    task 4

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,20 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int n = Integer.parseInt(args[0]);
+		String sen = (+ n + " is a perfect number since " + n + " = 1") ;
+		int numbers = 1 ;
+		
+		for (int i = 2 ; i<n ; i++) {
+			if (n % i == 0) {
+			numbers = numbers + i ;
+			sen = (sen + " + " + i) ;
+			}
+		}
+			if (numbers == n) {
+				System.out.println(sen);
+			} else {
+				System.out.println(n + " is not a perfect number");
+			}
 	}
 }
