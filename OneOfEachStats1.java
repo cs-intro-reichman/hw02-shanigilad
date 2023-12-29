024299745830890d431d4282e24bc599de89cfa3
Shani <shani.gilad@post.runi.ac.il>
2023-12-29 14:51:30 +0200

    question 6

--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -6,6 +6,36 @@
  */
 public class OneOfEachStats1 {
 	public static void main (String[] args) {
-		//// Put your code here
+		int b = 0 ; 
+		int g = 0 ; 
+		String child = "" ; 
+
+		double f = Math.random() ; // 0.0-0.999
+		if (f > 0.5) {
+			b++ ; 
+			child = child + "b " ;
+		} else if (f < 0.5) {
+			g++ ; 
+			child = child + "g " ;
+
+		}
+		while ((b == 0) || (g == 0)) {
+			f = Math.random() ;
+			if (f > 0.5) {
+			b++ ; 
+			child = child + "b " ;
+
+		} else if (f < 0.5) {
+			g++ ; 
+			child = child + "g " ;
+
+		}
+		}
+		int sum = b + g ;
+		System.out.println(child);
+		System.out.println("You made it... and you now have " + sum + " children." );
+
+
+
 	}
 }
