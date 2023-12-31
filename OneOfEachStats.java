6f9b3beb13e74963bb7544effd3f7314a3ee9db5
Shani <shani.gilad@post.runi.ac.il>
2023-12-31 12:28:38 +0200

    final version

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -9,12 +9,77 @@ import java.util.Random;
  */
 public class OneOfEachStats {
 	public static void main (String[] args) {
-		// Gets the two command-line arguments
 		int T = Integer.parseInt(args[0]);
 		int seed = Integer.parseInt(args[1]);
-		// Initailizes a random numbers generator with the given seed value
         Random generator = new Random(seed);  
-		
+		int sumchild = 0 ;
+		int n2 = 0 ;
+		int n3 = 0 ;
+		int n4 = 0 ;
+
+		for(int i = 0 ; i < T ; i++) {
+
+			int b = 0 ; 
+			int g = 0 ;
+
+			double f = generator.nextDouble(); ; // 0.0-0.999
+				if (f > 0.5) {
+					b++ ; 
+				} else if (f < 0.5) {
+					g++ ; 
+
+				}
+				while ((b == 0) || (g == 0)) {
+				f = generator.nextDouble(); ;
+				if (f > 0.5) {
+				b++ ; 
+				} else if (f < 0.5) {
+				g++ ; 
+				}
+				}
+
+				int sum = b + g ; 
+					if (sum == 2) {
+						n2++ ;
+					}
+					else if (sum == 3) {
+						n3++ ; 
+					}
+					else { 
+						n4++ ;
+					}
+					sumchild = sumchild + sum ;
+					}
+					
+
+						double average = ((double) sumchild / T) ; 
+						System.out.println("Average: " + average + " children to get at least one of each gender.") ;
+
+						System.out.println("Number of families with 2 children: " + n2) ; 
+						System.out.println("Number of families with 3 children: " + n3) ; 
+						System.out.println("Number of families with 4 or more children: " + n4) ; 
+
+						if (n2 > n3 && n2 > n4) {
+							System.out.println("The most common number of children is 2.") ; 
+						}
+						else if (n3 > n2 && n3 > n4) {
+						System.out.println("The most common number of children is 3.") ; 
+						}
+						else if (n4 > n2 && n4 > n3) {
+						System.out.println("The most common number of children is 4 or more.") ; 
+						} 
+						else if (n2 == n3) {
+						System.out.println("The most common number of children is 2.") ; 
+						}
+						else if (n2 == n4) {
+						System.out.println("The most common number of children is 2.") ; 
+						}
+						else if (n3 == n4) {
+						System.out.println("The most common number of children is 3.") ; 
+						}
+						else if (n2 == n3 && n3 == n4) {
+						System.out.println("The most common number of children is 2.") ; 
+						}
 		//// In the previous version of this program, you used a statement like:
 		//// double rnd = Math.random();
 		//// Where "rnd" is the variable that stores the generated random value.
@@ -25,5 +90,14 @@ public class OneOfEachStats {
 		//// randomization will be based on the given seed.
 		//// This is the only change that you have to do in the program.
 		    
+		}
 	}
-}
+
+		
+
+
+
+
+
+		
+	
--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -6,36 +6,74 @@
  */
 public class OneOfEachStats1 {
 	public static void main (String[] args) {
-		int b = 0 ; 
-		int g = 0 ; 
-		String child = "" ; 
+		int T = Integer.parseInt(args[0]);
+		int sumchild = 0 ;
+		int n2 = 0 ;
+		int n3 = 0 ;
+		int n4 = 0 ;
 
-		double f = Math.random() ; // 0.0-0.999
-		if (f > 0.5) {
-			b++ ; 
-			child = child + "b " ;
-		} else if (f < 0.5) {
-			g++ ; 
-			child = child + "g " ;
+		for(int i = 0 ; i < T ; i++) {
 
+			int b = 0 ; 
+			int g = 0 ;
+
+			double f = Math.random() ; // 0.0-0.999
+				if (f > 0.5) {
+					b++ ; 
+				} else if (f < 0.5) {
+					g++ ; 
+
+				}
+				while ((b == 0) || (g == 0)) {
+				f = Math.random() ;
+				if (f > 0.5) {
+				b++ ; 
+				} else if (f < 0.5) {
+				g++ ; 
+				}
+				}
+
+				int sum = b + g ; 
+					if (sum == 2) {
+						n2++ ;
+					}
+					else if (sum == 3) {
+						n3++ ; 
+					}
+					else { 
+						n4++ ;
+					}
+					sumchild = sumchild + sum ;
+					}
+					
+
+						double average = ((double) sumchild / T) ; 
+						System.out.println("Average: " + average + " children to get at least one of each gender.") ;
+
+						System.out.println("Number of families with 2 children: " + n2) ; 
+						System.out.println("Number of families with 3 children: " + n3) ; 
+						System.out.println("Number of families with 4 or more children: " + n4) ; 
+
+						if (n2 > n3 && n2 > n4) {
+							System.out.println("The most common number of children is 2.") ; 
+						}
+						else if (n3 > n2 && n3 > n4) {
+						System.out.println("The most common number of children is 3.") ; 
+						}
+						else if (n4 > n2 && n4 > n3) {
+						System.out.println("The most common number of children is 4 or more.") ; 
+						} 
+						else if (n2 == n3) {
+						System.out.println("The most common number of children is 2.") ; 
+						}
+						else if (n2 == n4) {
+						System.out.println("The most common number of children is 2.") ; 
+						}
+						else if (n3 == n4) {
+						System.out.println("The most common number of children is 3.") ; 
+						}
+						else if (n2 == n3 && n3 == n4) {
+						System.out.println("The most common number of children is 2.") ; 
+						}
 		}
-		while ((b == 0) || (g == 0)) {
-			f = Math.random() ;
-			if (f > 0.5) {
-			b++ ; 
-			child = child + "b " ;
-
-		} else if (f < 0.5) {
-			g++ ; 
-			child = child + "g " ;
-
 		}
-		}
-		int sum = b + g ;
-		System.out.println(child);
-		System.out.println("You made it... and you now have " + sum + " children." );
-
-
-
-	}
-}
